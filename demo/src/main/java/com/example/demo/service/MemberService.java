91df006fa0d61ca5438cf150d7c1c1a1f6ffcf2b
stopmin <geemin0@naver.com>
2023-09-19 03:02:20 +0900

    feat: h2 디비 연결 전까지

--- demo/src/main/java/com/example/demo/service/MemberService.java
+++ demo/src/main/java/com/example/demo/service/MemberService.java
@@ -5,6 +5,7 @@ import com.example.demo.repository.MemberRepository;
 import com.example.demo.repository.MemoryMemberRepository;
 import java.util.List;
 import java.util.Optional;
+import org.springframework.stereotype.Service;
 
 public class MemberService {
 
@@ -26,10 +27,9 @@ public class MemberService {
   }
 
   private void validateDuplicateMember(Member member) {
-    memberRepository.findByName(member.getName())
-        .ifPresent(m -> {
-          throw new IllegalStateException("이미 존재하는 회원입니다.");
-        });
+    memberRepository.findByName(member.getName()).ifPresent(m -> {
+      throw new IllegalStateException("이미 존재하는 회원입니다.");
+    });
   }
 
   /**
