10dab73cdffbd75d999f380a9fd29eec665d2f54
stopmin <geemin0@naver.com>
2023-10-02 15:53:30 +0900

    feat: 싱글톤 컨테이너

--- core/src/test/java/com/hello/core/singleton/SingletonTest.java
+++ core/src/test/java/com/hello/core/singleton/SingletonTest.java
@@ -2,9 +2,12 @@ package com.hello.core.singleton;
 
 import com.hello.core.AppConfig;
 import com.hello.core.member.MemberService;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 
 public class SingletonTest {
     @Test
@@ -23,6 +26,37 @@ public class SingletonTest {
         System.out.println("memberService2 = " + memberService2);
 
         // memberService != memberService2
-        Assertions.assertThat(memberService1).isNotSameAs(memberService2);
+        assertThat(memberService1).isNotSameAs(memberService2);
+    }
+
+    @Test
+    @DisplayName("싱글톤 패턴을 적용한 객체 사용")
+    void singletonServiceTest() {
+        SingletonService singletonService1 = SingletonService.getInstance();
+        SingletonService singletonService2 = SingletonService.getInstance();
+
+        System.out.println("singletonService1 = " + singletonService1);
+        System.out.println("singletonService2 = " + singletonService2);
+
+        assertThat(singletonService1).isSameAs(singletonService2);
+        // same ==
+        // equ
+    }
+
+    @Test
+    @DisplayName("스프링 컨테이너와 싱글톤")
+    void springContainer() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+
+        MemberService memberService1 = ac.getBean("memberService", MemberService.class);
+        MemberService memberService2 = ac.getBean("memberService", MemberService.class);
+
+        // 참조 값이 다른 것을 확인
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
+
+        // memberService == memberService2
+        assertThat(memberService1).isSameAs(memberService2);
     }
 }
