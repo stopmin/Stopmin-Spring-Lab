1e193fe0690ffa2958f25554b3ea46eed3a92169
stopmin <geemin0@naver.com>
2023-12-31 21:34:38 +0900

    chore: sequence 매핑

--- ex1-hello-jpa/src/main/java/jpa_basic/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/jpa_basic/hellojpa/JpaMain.java
@@ -1,22 +1,19 @@
 package jpa_basic.hellojpa;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
+import javax.persistence.*;
 
 public class JpaMain {
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
         EntityManager em = emf.createEntityManager();
         EntityTransaction tx = em.getTransaction();
+
         tx.begin();
 
         try {
             Member member = new Member();
-            member.setId(2L);
-            member.setUsername("B");
-            member.setRoleType(RoleType.GUEST);
+//            member.setId(2L);
+            member.setUsername("Yoon");
 
             em.persist(member);
 
@@ -27,6 +24,8 @@ public class JpaMain {
             em.close();
         }
 
+
+        // Closing EntityManagerFactory
         emf.close();
     }
 }
--- ex1-hello-jpa/src/main/java/jpa_basic/hellojpa/Member.java
+++ ex1-hello-jpa/src/main/java/jpa_basic/hellojpa/Member.java
@@ -1,48 +1,35 @@
 package jpa_basic.hellojpa;
 
 import javax.persistence.*;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.Date;
 
 @Entity
+@SequenceGenerator(name = "member_seq_generator", sequenceName = "member_seq")
 public class Member {
+
     @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "member_seq_generator")
     private Long id;
     @Column(name = "name", nullable = false)
     private String username;
-    private int age;
-    @Enumerated(EnumType.STRING)
-    private RoleType roleType;
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date createdDate;
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date lastModifiedDate;
-    private LocalDate testLocalDate;
-    private LocalDateTime testLocalDateTime;
-    @Lob
-    private String description;
 
-    @Transient
-    private int temp;
-
-    public Member() {
-
+    public Long getId() {
+        return id;
     }
-
 
     public void setId(Long id) {
         this.id = id;
     }
 
+    public String getUsername() {
+        return username;
+    }
 
     public void setUsername(String username) {
         this.username = username;
     }
 
+    public Member() {
 
-    public void setRoleType(RoleType roleType) {
-        this.roleType = roleType;
     }
 
 
