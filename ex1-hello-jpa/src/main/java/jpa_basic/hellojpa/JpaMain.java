836aa63618e0496ffdea31d27f0522cb4375dd8b
stopmin <geemin0@naver.com>
2024-01-02 01:14:59 +0900

    chore: gitignore 수정

--- ex1-hello-jpa/src/main/java/jpa_basic/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/jpa_basic/hellojpa/JpaMain.java
@@ -11,10 +11,10 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Member member = new Member();
-            member.setUsername("C");
-
-            em.persist(member);
+//            Member member = new Member();
+//            member.setUsername("C");
+//
+//            em.persist(member);
 
             tx.commit();
         } catch (Exception e) {
--- ex1-hello-jpa/src/main/java/jpa_basic/hellojpa/Member.java
+++ ex1-hello-jpa/src/main/java/jpa_basic/hellojpa/Member.java
@@ -7,6 +7,7 @@ public class Member {
 
     @Id
     @GeneratedValue
+    @Column(name = "MEMBER_ID")
     private Long id;
     @Column(name = "USERNAME")
     private String username;
