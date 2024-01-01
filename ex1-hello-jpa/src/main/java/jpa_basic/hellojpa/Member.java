9e83420924a3ba6f4a22418c1b271044ccda9554
stopmin <geemin0@naver.com>
2024-01-02 01:12:55 +0900

    chore:domain 수정(team추가)

--- ex1-hello-jpa/src/main/java/jpa_basic/hellojpa/Member.java
+++ ex1-hello-jpa/src/main/java/jpa_basic/hellojpa/Member.java
@@ -3,38 +3,13 @@ package jpa_basic.hellojpa;
 import javax.persistence.*;
 
 @Entity
-@SequenceGenerator(
-        name = "MEMBER_SEQ_GENERATOR",
-        sequenceName = "MEMBER_SEQ",//매핑할 데이터베이스 시퀀스 이름
-        initialValue = 1, allocationSize = 50)
 public class Member {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE,
-            generator = "MEMBER_SEQ_GENERATOR")
+    @GeneratedValue
     private Long id;
-    @Column(name = "name", nullable = false)
+    @Column(name = "USERNAME")
     private String username;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public Member() {
-
-    }
-
-
+    @Column(name = "TEAM_ID")
+    private Long teamId;
 }
---
+++ ex1-hello-jpa/src/main/java/jpa_basic/hellojpa/Team.java
@@ -0,0 +1,14 @@
+package jpa_basic.hellojpa;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Team {
+    @Id
+    @GeneratedValue
+    @Column(name = "TEAM_ID")
+    private Long id;
+}
