759eaa18a165c3b6801c18b124d2ca98f3671b6a
stopmin <geemin0@naver.com>
2024-01-05 15:54:12 +0900

    chore: 상품 도메인 개발

--- jpashop/src/main/java/jpabook/jpashop/repository/ItemRepository.java
+++ jpashop/src/main/java/jpabook/jpashop/repository/ItemRepository.java
@@ -5,6 +5,8 @@ import jpabook.jpashop.domain.item.Item;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 @RequiredArgsConstructor
 public class ItemRepository {
@@ -16,5 +18,14 @@ public class ItemRepository {
         } else {
             em.merge(item);
         }
+    }
+
+    public Item findOne(Long id) {
+        return em.find(Item.class, id);
+    }
+
+    public List<Item> findAll() {
+        return em.createQuery("select i from Item i", Item.class)
+                .getResultList();
     }
 }
