4b1c003842cea74572e1040c17f98e3852ad45a5
stopmin <geemin0@naver.com>
2024-02-26 16:45:26 +0900

    -

--- core/src/test/java/com/hello/core/singleton/StatefulService.java
+++
@@ -1,9 +0,0 @@
-package com.hello.core.singleton;
-
-public class StatefulService {
-    //    private int price; // 상태를 유지하는 필드
-    public int order(String name, int price) {
-        System.out.println("name = " + name + " price = " + price);
-        return price;
-    }
-}
