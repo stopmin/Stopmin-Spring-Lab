7c888f05af4b7b225433fd4f30bf81409da3e62e
stopmin <geemin0@naver.com>
2024-09-27 15:41:31 +0900

    상품수정

--- item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -72,6 +72,20 @@ public class BasicItemController {
         return "basic/item";
     }
 
+    @GetMapping("/{itemId}/edit")
+    public String editForm(@PathVariable Long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "basic/editForm";
+    }
+
+    @PostMapping("/{itemId}/edit")
+    public String edit(@PathVariable Long itemId, Model model) {
+//        Item item = itemRepository.findById(itemId);
+//        model.addAttribute("item", item);
+        return "redirect:/basic/items/{itemId}";
+    }
+
     @PostConstruct
     public void init() {
 
